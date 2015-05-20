5178af118dfc0a6615306e05505cde0f2ee2e5bb
cgamfcg <dmcgam@gmail.com>
2015-05-20 20:47:28 +0900

    init directories

---
+++ MyApp/Models/Detail.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MyApp.Models
+{
+    [Table("detail", Schema = "public")]
+    public class Details
+    {
+        [Key]
+        [DisplayName("ヘッダーID")]
+        [Column("id", Order=1)]
+        [Required]
+        public int Id { get; set; }
+
+        [Key]
+        [DisplayName("ID")]
+        [Column("id", Order = 1)]
+        [Required]
+        [MaxLength(256)]
+        public string Title { get; set; }
+
+        [DisplayName("詳細")]
+        [Column("description")]
+        [MaxLength(2)]
+        public string Description { get; set; }
+
+        [DisplayName("備考")]
+        [Column("caution")]
+        public DateTime Caution { get; set; }
+
+        [DisplayName("作成日時")]
+        [Column("createdat")]
+        public DateTime Createdat { get; set; }
+
+        [DisplayName("更新日時")]
+        [Column("updatedat")]
+        public DateTime Updatedat { get; set; }
+
+    }
+}
---
+++ MyApp/Models/Header.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MyApp.Models
+{
+    [Table("main", Schema = "public")]
+    public class Header
+    {
+        [Key]
+        [DisplayName("ID")]
+        [Column("id")]
+        [Required]
+        public int Id { get; set; }
+
+        [DisplayName("タイトル")]
+        [Column("title")]
+        [MaxLength(256)]
+        public string Title { get; set; }
+
+        [DisplayName("何らかの区分")]
+        [Column("appdiv")]
+        [MaxLength(2)]
+        public string Appdiv { get; set; }
+
+        [DisplayName("作成日時")]
+        [Column("createdat")]
+        public DateTime Createdat { get; set; }
+
+        [DisplayName("更新日時")]
+        [Column("updatedat")]
+        public DateTime Updatedat { get; set; }
+
+    }
+}
